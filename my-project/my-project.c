9278d372f0d94b420be39428b42ac4a9dbd53335
noctrog <ramon1999cg@gmail.com>
2018-12-08 17:51:56 +0100

    Added FreeRTOS

--- my-project/my-project.c
+++ my-project/my-project.c
@@ -1,6 +1,12 @@
-#include "api.h"
+#include <FreeRTOS.h>
+#include <queue.h>
+#include <task.h>
+
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/usart.h>
+#include <libopencm3/stm32/gpio.h>
 
 int main(void) {
 	/* add your own code */
-	return my_func(3);
+
 }
